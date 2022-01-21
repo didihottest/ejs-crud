03cf239e98fc3bb06f7f288774236d4d2a1aceb5
didihottest <didihottest@gmail.com>
2022-01-21 22:53:18 +0700

    update login menggunakan json web token

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const { uuid } = require('uuidv4');
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
+const isLoggedIn = require('./middleware/authMiddleware')
 
+app.use(cookieParser())
 app.set('view engine', 'ejs')
 app.set('views', './public/views')
 // untuk share file secara public
@@ -11,7 +15,7 @@ app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get('/', isLoggedIn, (req, res) => {
   const data = fs.readFileSync('./data/data.json', 'utf-8')
   const dataParsed = JSON.parse(data)
   res.render('main', {
@@ -20,7 +24,7 @@ app.get('/', (req, res) => {
   })
 })
 
-app.get('/add', (req, res) => {
+app.get('/add', isLoggedIn, (req, res) => {
   res.render('add.ejs', {
     pageTitle: "Add",
   })
@@ -41,7 +45,7 @@ app.post('/add', (req, res) => {
   res.redirect('/')
 })
 
-app.get('/edit', (req, res) => {
+app.get('/edit', isLoggedIn, (req, res) => {
   const { id } = req.query
   const data = fs.readFileSync('./data/data.json', 'utf-8')
   const dataParsed = JSON.parse(data)
@@ -116,6 +120,66 @@ app.post('/delete', (req, res) => {
   })
   fs.writeFileSync('./data/data.json', JSON.stringify(deletedList, null, 4))
   res.redirect('/')
+})
+
+app.get('/login', (req, res) => {
+  const { status } = req.query
+  res.render('login', {
+    status
+  })
+})
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body
+  const data = JSON.parse(fs.readFileSync('./data/user.json', 'utf-8'))
+  const userMatch = data.find((item) => item.email == email)
+
+  // if userMatch === null atau userMatch === undefined atau userMatch === false
+  if (!userMatch) {
+    res.redirect('/login?status=emailnotfound')
+  } else {
+    if (password === userMatch.password) {
+      const token = jwt.sign({ //ngunci data (in this case email & user)
+        email: userMatch.email,
+        id: userMatch.id
+      }, 'secret', {
+        // expiresIn: 60 * 60 * 24 // 1 hari satuan detik
+        expiresIn: 86400 // 1 hari
+        // 60 detik dikali 60 detik = 3600 detik = 1 jam
+        // 1 jam dikali 24 = 1 hari
+      })
+
+      // res.cookie('jwt', token, { maxAge: 1000 * 60 * 60 * 24 })// max age satu hari
+      res.cookie('jwt', token, { maxAge: 86400000 })// max age satu hari (satuan milisecon)
+      res.redirect('/')
+    } else {
+      res.redirect('/login?status=wrongpassword')
+    }
+  }
+})
+
+
+app.get('/set-cookies', (req, res) => {
+  // cara vanilla
+  // res.setHeader('Set-Cookie', 'userId=1')
+  // cara modul cookieParser
+  res.cookie('userId', 1)
+  res.cookie('username', "Didi", { maxAge: 1000 * 60 * 60 * 24 })
+  // max age cookie gunanya untuk masa waktu cookie di dalam browser
+  // kalau waktunya habis maka cookie nya akan menghilang
+  res.json({
+    message: "anda mendapat cookie"
+  })
+})
+
+app.get('/get-cookies', (req, res) => {
+  console.log(req.cookies)
+  res.json({ cookies: req.cookies })
+})
+
+app.post('/logout', (req, res) => {
+  res.cookie('jwt', '', { maxAge: 5000 })
+  res.redirect('/login')
 })
 
 
